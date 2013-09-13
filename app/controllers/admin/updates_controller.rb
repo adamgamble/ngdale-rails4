f8039bd6fe7b2d7af571292a646bdd80ffedf54e
Adam Gamble <adamgamble@gmail.com>
2013-09-13 01:45:43 -0500

    fix route

--- app/controllers/admin/updates_controller.rb
+++ app/controllers/admin/updates_controller.rb
@@ -9,9 +9,9 @@ class Admin::UpdatesController < Admin::ApplicationController
   end
 
   def phone_call
-    response = Twilio::TwiML::Response.new do |r|
+    twilml = Twilio::TwiML::Response.new do |r|
       r.Say 'hello there', :voice => 'alice'
     end
-    render text: response
+    render text: twilml
   end
 end
