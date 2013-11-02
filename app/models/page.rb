3c2bb0a8841054bf371a8ecad4d1b9eb4182c236
Adam Gamble <adamgamble@gmail.com>
2013-11-02 11:57:49 -0500

    added smaller images to page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
-  has_attached_file :image
+  has_attached_file :image, :styles => { :medium => "500x360>", :small => "250x180>" }
   def name
     read_attribute("name") || ""
   end
