8b35860d6084bd11c7634572df55363812a91ee7
Adam Gamble <adamgamble@gmail.com>
2013-11-10 22:14:10 -0600

    Added preacher to sermons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131101041920) do
+ActiveRecord::Schema.define(version: 20131111041342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20131101041920) do
     t.string   "audio_content_type"
     t.integer  "audio_file_size"
     t.datetime "audio_updated_at"
+    t.string   "preacher"
   end
 
   create_table "updates", force: true do |t|
