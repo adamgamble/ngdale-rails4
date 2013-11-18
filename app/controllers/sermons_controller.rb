0ce4bf68274c0f8763632d3b2c46395954b0dcb5
Adam Gamble <adamgamble@gmail.com>
2013-11-17 21:17:45 -0600

    Added sermons index

--- app/controllers/sermons_controller.rb
+++ app/controllers/sermons_controller.rb
@@ -1,5 +1,9 @@
 class SermonsController < ApplicationController
 
+  def index
+    @sermons = Sermon.order(:date => :desc).page(params[:page])
+  end
+
   def show
     @sermon = Sermon.find(params[:id])
   end
